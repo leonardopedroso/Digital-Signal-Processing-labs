112c948a92f7f1c3d7be8fb02a8458b882dc802b
LeonardoPedroso <leonardo.pedroso@tecnico.ulisboa.pt>
2021-05-20 01:08:51 +0100

    lab 4 leo

--- lab4/anomalyR2a.m
+++ lab4/anomalyR2a.m
@@ -1,14 +1,9 @@
 function [anomaly] = anomalyR2a(x,x_pred,thrshl)
-    
     anomaly = zeros(length(x),1);
-    for i = 1:length(x)
-        %if((x_pred(i)-x(i))/mean(abs(x)) > epsl)
-        %   anomaly(i) = 1; 
-        %end
-        if(abs(x_pred(i)-x(i)) > thrshl)
-            anomaly(i) = 1;
+    for i = 1:length(x) % Iterate through the elements of the vector
+        if(abs(x_pred(i)-x(i)) > thrshl) % if difference is abovce threshold
+            anomaly(i) = 1; % Set anomaly vector to true
         end
     end
-
 end
 
